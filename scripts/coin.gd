38eb314df63251f991190449d5cac2f2cdb129e9
Irene26at <151548977+Irene26at@users.noreply.github.com>
2024-08-18 11:45:38 +0530

    Add files via upload

---
+++ scripts/coin.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
+	animation_player.play("pickup_animation")
---
+++ scripts/killzone.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(body: Node2D) -> void:
+	print("You died!") # Replace with function body.
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
